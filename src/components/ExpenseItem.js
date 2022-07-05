39ccb608004d5d532fc6f72a9971659d877c448f
Lukáš Míšek <mishek@gmail.com>
2022-07-05 18:51:59 +0200

    adding css

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,15 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
-
-return <h2>Expense Item!!</h2>
-
-
+    return (
+        <div className="expense-item">
+            <div>Date</div>
+            <div className='expense-item__description'>
+                <h2>Title</h2>
+                <div className='expense-item__price'>Amount</div>
+            </div>
+        </div>
+    )
 }
 
 export default ExpenseItem;
