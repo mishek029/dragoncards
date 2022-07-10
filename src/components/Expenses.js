3e5dbb7b31ed067f05dd2d7a2aceab295cc9f89d
Lukáš Míšek <mishek@gmail.com>
2022-07-10 11:43:06 +0200

    CARD

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,4 +1,5 @@
 import ExpenseItem from "./ExpenseItem";
+import Card from './Card';
 import "./Expenses.css";
 
 function Expenses(props) {
@@ -14,9 +15,9 @@ function Expenses(props) {
     )
 
     return (
-        <div className="expenses">
+        <Card className="expenses">
             {expensesComponents}
-        </div>
+        </Card>
 
     )
 }
